c18486bad142e6645c2adcdbe58098d62aca571d
zusoomro <zusoomro@seas.upenn.edu>
2021-10-10 23:00:32 -0400

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/help', to: 'static_pages#help'
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
   root 'static_pages#home'
 end
